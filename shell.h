0e5f5ff7f7a0f30832418b629469d76e11816616
Crenacia <hlalalamaluleke@gmail.com>
2023-04-19 12:54:10 -0700

    shell,h

---
+++ shell.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+
+int interactive(info_t*info);
+int _myexit (info_t*info);
+int _myhistory (inf0_t *info);
+int _mynv (info_t *info);
+int _putfd(char c, int fd);
+
+#endif
